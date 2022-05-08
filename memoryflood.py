04470c9d575c2b47e3474316367121572b5b4de3
ferrevdd <ferre.vandendriessche@gmail.com>
2022-05-08 19:43:24 +0200

    Update memoryflood.py

--- memoryflood.py
+++ memoryflood.py
@@ -1,27 +1,29 @@
-import random , psutil , time, os, sys
-from threading import local
-from tkinter import Variable
-from click import pause
-list = []
+import psutil , time, os, sys
 from multiprocessing import Process, Queue
+
 def cls():                                         
     os.system('cls' if os.name=='nt' else 'clear')  
 
 def tasker(memory,listname,processname,q):
     
     listname = []
+    
     while (psutil.Process().memory_info().rss / (1024 * 1024)) < int(memory):
+       
         randomstrink = 1
+        
         for i in range(10000000):
             listname.append(randomstrink)
         
     print(f'thread {processname} done , total mem: {round(psutil.Process().memory_info().rss / (1024 * 1024))} megabytes')    
+    
     q.put(f'{processname}')
+    
     time.sleep(99999)
 
 if __name__ == '__main__':
-    q = Queue()
     
+    q = Queue()
     
     if len(sys.argv) == 1:
         
@@ -36,34 +38,35 @@ if __name__ == '__main__':
     
     else:
         memfloodamount = sys.argv[1]
+        
         print(f'memory limit set to {memfloodamount}, starting to flood in 1 sec...')
         time.sleep(1)
         cls()
     
     partedmem = int(memfloodamount) /8
+    
     process1 ='';process2 = '';process3 = '';process4 = '';process5 = '';process6 = '';process7 = '';process8 = ''
     
     testeke = [process1,process2,process3,process4,process5,process6,process7,process8]
     
     for i in range(len(testeke)):
         testeke[i] = Process(target=tasker, args=(partedmem,f'list{i}',f'process{i}',q))
+        
         testeke[i].start()
+        
         print(testeke[i])
     
-    
     time.sleep(3)    
+    
     cls()
+    
     print('all child processes are working... please wait...')
 
-    
-    
-    
     StatusChecker = []
+    
     for i in range(8):
         
         StatusChecker.append(q.get())
-    
-    
     
     if len(StatusChecker) == 8:
         print('Received DONE signal from all child processes')
@@ -72,19 +75,28 @@ if __name__ == '__main__':
         print('something went wrong, quitting in 5 sec...')
         time.sleep(5)
         quit()
+    
     time.sleep(3)
     cls()
+    
     ghost = input('press enter to release memory...')
     cls()
+    
     for i in range(len(testeke)):
+        
         testeke[i].terminate()
+        
         time.sleep(1)
+        
         print(f'{testeke[i]}      |||continuing to next child...')
     
     
     time.sleep(3)
+    
     cls()
+    
     print('released memory sucessfully, press enter to quit...')
+    
     dede = input()
     
     
