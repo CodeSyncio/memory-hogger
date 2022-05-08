a0a1508c7124f332f0184af11d3bb2f752b885a0
ferrevdd <ferre.vandendriessche@gmail.com>
2022-05-08 13:49:06 +0200

    argument support

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from ast import Delete
-import random , psutil , time, os
+import random , psutil , time, os, sys
 list = []
 
 def cls():                                         
     os.system('cls' if os.name=='nt' else 'clear')  
-
-memfloodamount = input('amount of memory to pump full (MB):\n')
-print(f'memory limit set to {memfloodamount} , press enter to continue...')
-cls()
-print('pumping will start in 3 seconds...')
-time.sleep(3)
-cls()
-print('Pumping...')
-
+if len(sys.argv) == 1:
+     
+    memfloodamount = input('amount of memory to pump full (MB):\n')
+    print(f'memory limit set to {memfloodamount} , press enter to continue...')
+    ghosty = input()
+    cls()
+    print('pumping will start in 3 seconds...')
+    time.sleep(3)
+    cls()
+    
+else:
+    memfloodamount = sys.argv[1]
+    print(f'memory limit set to {memfloodamount}, starting to flood in 1 sec...')
+    time.sleep(1)
+    cls()
+    
 while (psutil.Process().memory_info().rss / (1024 * 1024)) < int(memfloodamount):
     randomstrink = 1
     for i in range(10000000):
